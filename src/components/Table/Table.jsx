9e42b07449192139f89f5ab1cba3ce66242f7cd0
tonistorres <sisvenda2011@gmail.com>
2023-01-16 19:14:38 -0300

    feat: adding github

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -7,7 +7,6 @@ import './Table.css';
 
 function Table() {
   const value = useContext(CrudContext);
-
   return (
     <div className="container-fluid d-flex align-items-center justify-content-center mt-4 mb-4">
       <table className='table-responsive-sm table-responsive-md table-responsive-lg table-responsive-xl'>
@@ -43,12 +42,210 @@ function Table() {
                     <td className=" border bg-light p-2">{item.dad}</td>
                     <td className=" border bg-light p-2">{item.mother}</td>
                     <td className=" border bg-light p-2">{item.status}</td>
-                    <td className=" border bg-light p-2"><FiEdit3 size={36} /></td>
                     <td className=" border bg-light p-2">
+                      {/* Editar Registro  */}
+                      {/**O Botão Captura dados relacionados ao campo que recebe o evento de click */}
+                      <button
+                        type="button"
+                        className="btn"
+                        data-toggle="modal"
+                        data-target="#updateModal"
+                        onClick={() => value.handleFindId(item.register)}>
+                        <FiEdit3 size={36} color="#563D7C" />
+                      </button>
+                      <div className="modal fade" id="updateModal" tabIndex="-1" role="dialog" aria-labelledby="updateModalLabel" aria-hidden="true">
+                        <div className="modal-dialog" role="document">
+                          <div className="modal-content">
+                            <div className="modal-header">
+                              <h5 className="modal-title" id="updateModalLabel">Deseja Atualizar Registro</h5>
+                            </div>
+                            <div className="modal-body">
+                              <div className="modal-body">
+                                <form>
+                                  <div className="form-group">
+                                    <div className="row">
+                                      <div className="col-4">
+                                        <label htmlFor="recipient-register" className="col-form-label">Register:</label>
+                                        <input
+                                          type="text"
+                                          className="form-control"
+                                          id="recipient-register"
+                                          name="register"
+                                          value={value.register}
+                                          disabled={true}
+                                        />
+                                      </div>
 
+
+                                      <div className="col-8">
+                                        <label htmlFor="recipient-name" className="col-form-label">Membro:</label>
+                                        <input
+                                          type="text"
+                                          className="form-control"
+                                          placeholder='ex: Antonio da Silva'
+                                          id="recipient-name"
+                                          name="name"
+                                          value={value.name}
+                                          onChange={value.handleChangeName}
+                                        />
+                                      </div>
+
+                                    </div>
+
+                                    <div className="row">
+                                      <div className="col-4 ">
+                                        <label htmlFor="recipient-ano" className="col-form-label">Ano Batismo:</label>
+                                        <input
+                                          type="number"
+                                          className="form-control w-100"
+                                          placeholder='0'
+                                          id="recipient-ano"
+                                          name="baptism"
+                                          value={value.baptism}
+                                          onChange={value.handleChangeBaptism}
+                                        />
+                                      </div>
+                                      <div className="col-4 ">
+                                        <label htmlFor="recipient-birth" className="col-form-label">Nascimento:</label>
+                                        <input
+                                          type="string"
+                                          autoComplete="off"
+                                          className="form-control w-100"
+                                          maxLength={10}
+                                          id="recipient-birth"
+                                          name="birth"
+                                          value={value.birth}
+                                          onChange={value.handleChangeBirth}
+                                          onKeyUp={value.handleKeyUpBirth}
+                                          required={false}
+                                          style={{ backgroundColor: "#563D7C", color: "#ffffff", fontWeight: 700, fontSize: "18px" }}
+                                        />
+                                      </div>
+                                      <div className="col-4 ">
+                                        <label htmlFor="recipient-contry" className="col-form-label">País:</label>
+                                        <input
+                                          type="text"
+                                          className="form-control w-100"
+                                          id="recipient-country"
+                                          name="country"
+                                          value={value.country}
+                                          onChange={value.handleChangeCountry}
+                                        />
+                                      </div>
+                                    </div>
+
+                                    <div className="row">
+                                      <div className="col-4 ">
+                                        <label htmlFor="recipient-state" className="col-form-label">Estado:</label>
+                                        <input
+                                          type="text"
+                                          className="form-control w-100"
+                                          placeholder='ex:São Paulo'
+                                          id="recipient-state"
+                                          name="state"
+                                          value={value.state}
+                                          onChange={value.handleChangeState}
+                                        />
+                                      </div>
+                                      <div className="col-4 ">
+                                        <label htmlFor="recipient-city" className="col-form-label">Cidade:</label>
+                                        <input
+                                          type="text"
+                                          className="form-control w-100"
+                                          placeholder='ex: Cidade'
+                                          id="recipient-city"
+                                          name="city"
+                                          value={value.city}
+                                          onChange={value.handleChangeCity}
+                                        />
+                                      </div>
+                                      <div className="col-4 ">
+                                        <label htmlFor="recipient-district" className="col-form-label">Bairro:</label>
+                                        <input
+                                          type="text"
+                                          className="form-control w-100"
+                                          placeholder='ex:Bairro'
+                                          id="recipient-district"
+                                          name="district"
+                                          value={value.district}
+                                          onChange={value.handleChangeDistrict}
+                                        />
+                                      </div>
+                                    </div>
+
+                                    <div className="row">
+                                      <div className="col ">
+                                        <label htmlFor="recipient-dad" className="col-form-label">Pai:</label>
+                                        <input
+                                          type="text"
+                                          className="form-control w-100"
+                                          placeholder='ex: Jõao Silva'
+                                          id="recipient-dad"
+                                          name="dad"
+                                          value={value.dad}
+                                          onChange={value.handleChangeDad}
+                                        />
+                                      </div>
+                                    </div>
+                                    <div className="row">
+                                      <div className="col ">
+                                        <label htmlFor="recipient-mother" className="col-form-label">Mãe:</label>
+                                        <input
+                                          type="text"
+                                          className="form-control w-100"
+                                          placeholder='ex: Joana Silva'
+                                          id="recipient-mother"
+                                          name="mother"
+                                          value={value.mother}
+                                          onChange={value.handleChangeMother}
+                                        />
+                                      </div>
+                                    </div>
+
+                                    <div className="row">
+                                      <div className="col-4 ">
+                                        <label htmlFor="rec ipient-status" className="col-form-label">Situação Civil:</label>
+                                        <input
+                                          type="text"
+                                          className="form-control w-100"
+                                          placeholder='ex: Solteiro(a)'
+                                          id="recipient-status"
+                                          name="statuscivil"
+                                          value={value.status}
+                                          onChange={value.handleChangeStatus}
+                                        />
+                                      </div>
+                                    </div>
+                                  </div>
+                                </form>
+                              </div>
+                            </div>
+                            <div className="modal-footer">
+                              <button type="button" className="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                              <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={() => value.handleUpdate({
+                                "name": `${value.name}`,
+                                "baptism": `${value.baptism}`,
+                                "birth": `${value.treatingDate(value.birth)}`,
+                                "country": `${value.country}`,
+                                "state": `${value.state}`,
+                                "city": `${value.city}`,
+                                "district": `${value.district}`,
+                                "mother": `${value.mother}`,
+                                "dad": `${value.dad}`,
+                                "status": `${value.status}`
+                              })}  > Atualizar</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      {/* Fim Editar Registro */}
+                    </td>
+                    <td className=" border bg-light p-2">
+                      {/* Excluir Registros INICIO */}
                       <button type="button" className="btn" data-toggle="modal" data-target="#exampleModal" onClick={() => value.setCurrentIdDelete(item.register)}>
                         <BsFillTrash2Fill size={36} color="#563D7C" />
                       </button>
+
                       <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div className="modal-dialog" role="document">
                           <div className="modal-content">
@@ -66,6 +263,7 @@ function Table() {
                           </div>
                         </div>
                       </div>
+                      {/*Excluir Registros Fim*/}
                     </td>
                   </tr>
                 </tbody>
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,18 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import CrudContext from './CrudContext';
 import { urlBaseMembers } from "../../src/services/api";
-/**
- * Algo que poderia ser implementando nesse ponto é trabalhar um pouco mais com o state do reac
- *  no que se refere ao método handleAll no momento de deleção utilizar uma função que retorne
- * para o usuário no o que tem no banco mais o que foi tirado naquele momento do estado atual
- * ou o que tem no estado atual em vez de fazer uma requisição no banco
- */
 
 // eslint-disable-next-line react/prop-types
 function HomeProvider({ children }) {
 
   const [arrayAllMembers, setAllMembers] = useState([]);
   const [currentIdDelete, setCurrentIdDelete] = useState('');
+  const [currentIdUpdate, setCurrentIdUpdate] = useState('');
+
+  //update
+
+  const [register, setRegister] = useState('');
+  const [cep, setCep] = useState('65413-000');
+
+  const [name, setNameMember] = useState('');
+  const [baptism, setBaptism] = useState(0);
+  const [birth, setBirth] = useState('');
+  console.log('State Birth', birth);
+  const [country, setCountry] = useState('');
+  const [state, setState] = useState('');
+  const [city, setCity] = useState('');
+  const [district, setDistrict] = useState('');
+  const [dad, setDad] = useState('');
+  const [mother, setMother] = useState('');
+  const [status, setStatus] = useState('');
+
+
 
   useEffect(() => {
     const listAll = async () => {
@@ -38,6 +52,27 @@ function HomeProvider({ children }) {
     }
   }
 
+
+  const handleFindId = async (id) => {
+    try {
+      const response = await urlBaseMembers.get(`/members/${id}`);
+      console.log('Ex o dado filtrado:', response.data);
+      setRegister(response.data.register)
+      setNameMember(response.data.name);
+      setBaptism(response.data.baptism);
+      setBirth(response.data.birth);
+      setCountry(response.data.country);
+      setCity(response.data.city);
+      setDistrict(response.data.district);
+      setMother(response.data.mother);
+      setDad(response.data.dad);
+      setState(response.data.state);
+      setStatus(response.data.status);
+    } catch (error) {
+      console.log('Internal Error Method FindId');
+    }
+  }
+
   const handleCreate = async (newMemberObject) => {
     try {
       await urlBaseMembers.post(`/member`, newMemberObject);
@@ -56,20 +91,139 @@ function HomeProvider({ children }) {
     }
   }
 
+
+  const handleUpdate = async (objUpdate) => {
+    try {
+      await urlBaseMembers.put(`/members/${register}`, objUpdate);
+      await handleFindAll();
+    } catch (error) {
+      alert('Internal Error Method Update ', error)
+    }
+  }
+
+  // UPDATE
+
+  const handleChangeName = (evt) => {
+    setNameMember(evt.target.value);
+  }
+
+  const handleChangeBaptism = (evt) => {
+    setBaptism(evt.target.value);
+  }
+  // Cep
+  const handleKeyUpCEP = useCallback((evt) => {
+
+    evt.currentTarget.maxLenght = 9;
+    let value = evt.currentTarget.value;
+    value = value.replace(/\D/g, "");
+    value = value.replace(/^(\d{5})(\d)/, "$1-$2");
+    evt.currentTarget.value = value;
+  }, []);
+
+
+  const handleChangeCEP = (evt) => {
+    setCep(evt.target.value);
+  }
+
+  // Birth
+  const handleKeyUpBirth = useCallback((evt) => {
+    evt.currentTarget.maxLenght = 10;
+    let value = evt.currentTarget.value;
+    value = value.replace(/\D/g, "");
+    value = value.replace(/(\d{2})(\d{2})(\d{4})/, "$1/$2/$3");
+    evt.currentTarget.value = value;
+
+  }, []);
+
+  const handleChangeBirth = (evt) => {
+    setBirth(evt.target.value);
+
+  }
+
+
+  const handleChangeCountry = (evt) => {
+    setCountry(evt.target.value);
+
+  }
+
+  const handleChangeState = (evt) => {
+    setState(evt.target.value);
+  }
+
+
+  const handleChangeCity = (evt) => {
+    setCity(evt.target.value);
+  }
+
+  const handleChangeStatus = (evt) => {
+    setStatus(evt.target.value);
+  }
+
+
+  const handleChangeMother = (evt) => {
+    setMother(evt.target.value);
+  }
+
+  const handleChangeDad = (evt) => {
+    setDad(evt.target.value);
+  }
+
+  const handleChangeDistrict = (evt) => {
+    setDistrict(evt.target.value);
+  }
+  function treatingDate(str) {
+    const year = str.substring(4, 10);
+    const month = str.substring(3, 5);
+    const day = str.substring(0, 2);
+    return `${day}/${month}/${year}`
+  }
+
+
   return (
     <CrudContext.Provider value={{
+      handleKeyUpCEP,
+      handleKeyUpBirth,
       arrayAllMembers,
       currentIdDelete,
-      setCurrentIdDelete,
+      currentIdUpdate,
       handleFindAll,
       handleCreate,
+      handleDelete,
+      handleUpdate,
       setAllMembers,
-      handleDelete
-
+      setCurrentIdDelete,
+      setCurrentIdUpdate,
+      //UPDATE
+      handleChangeCEP,
+      handleChangeName,
+      handleChangeBaptism,
+      handleChangeBirth,
+      handleChangeStatus,
+      handleChangeMother,
+      handleChangeDad,
+      handleChangeDistrict,
+      handleChangeCountry,
+      handleChangeState,
+      handleChangeCity,
+      treatingDate,
+      handleFindId,
+      cep,
+      register,
+      name,
+      baptism,
+      birth,
+      country,
+      state,
+      city,
+      district,
+      dad,
+      mother,
+      status
     }}>
       {children}
     </CrudContext.Provider>
   );
 }
+
 
 export default HomeProvider;
